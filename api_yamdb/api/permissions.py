85260e016be082c6f13d92be6d84ad1ad1d2759b
Сергей Емельянов <3201888@mail.ru>
2023-04-26 16:04:07 +0300

    .

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -3,4 +3,22 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 class IsAdminOnly(BasePermission):
     def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.is_admin
+
+
+class ReadOnly(BasePermission):
+    def has_permission(self, request, view):
         return request.method in SAFE_METHODS
+
+
+class IsAuthorOrModeratorOrReadOnly(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return (
+            request.method in SAFE_METHODS
+            or request.user.is_authenticated
+            and (
+                request.user.is_admin
+                or request.user.is_moderator
+                or request.user == obj.author
+            )
+        )
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,8 +1,14 @@
-from rest_framework import viewsets
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from rest_framework import viewsets, filters, status
 from rest_framework.filters import SearchFilter
+from rest_framework.permissions import (
+    IsAuthenticated, IsAuthenticatedOrReadOnly, AllowAny
+)
 
 from reviews.models import User
-from .permissions import IsAdminOnly
+from .permissions import IsAdminOnly, ReadOnly, IsAuthorOrModeratorOrReadOnly
 from .serializers import UserSerializer
 
 
@@ -14,3 +20,19 @@ class UserViewSet(viewsets.ModelViewSet):
     search_fields = ('username',)
     lookup_field = 'username'
     http_method_names = ('get', 'post', 'head', 'patch', 'delete')
+
+    @action(
+        detail=False,
+        methods=('GET', 'PATCH'),
+        url_path='me',
+        permission_classes=(IsAuthenticated,),
+        serializer_class=UserSerializer
+    )
+    def me(self, request):
+        user = get_object_or_404(User, pk=request.user.id)
+        serializer = self.get_serializer(
+            user, data=request.data, partial=True
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save(role=user.role)
+        return Response(serializer.data, status=status.HTTP_200_OK)
