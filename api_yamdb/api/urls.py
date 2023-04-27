257838983662bec8977c271eac6ac929f9e3fe01
Артём Лашин <sntchweb@yandex.ru>
2023-04-27 13:57:02 +0400

    urls change

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -12,8 +12,16 @@ router_v1.register(r'users', UserViewSet, basename='users')
 router_v1.register(r'categories', CategoryViewSet)
 router_v1.register(r'genres', GenreViewSet)
 router_v1.register(r'titles', TitleViewSet)
-router_v1.register(r'comments', CommentViewSet, basename='comments')
-router_v1.register(r'reviews', ReviewViewSet, basename='reviews')
+router_v1.register(
+    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+    CommentViewSet,
+    basename='comments'
+)
+router_v1.register(
+    r'titles/(?P<title_id>\d+)/reviews',
+    ReviewViewSet,
+    basename='reviews'
+)
 
 urlpatterns = [
     path('v1/', include(router_v1.urls)),
